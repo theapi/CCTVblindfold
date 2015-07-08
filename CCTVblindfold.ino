2309128b2608fbc1de53631b39ccf0896307b0bb
theapi <petermclarke@gmail.com>
2015-07-08 09:50:12 +0100

    State machine

--- CCTVblindfold.ino
+++ CCTVblindfold.ino
@@ -1,48 +1,114 @@
 
-#define PIN_DIRECTION 2
-#define QUARTER_TURN 1024
+#define PIN_TOGGLE 2 // Used to switch between open & closed
+#define QUARTER_TURN 1024 // How many steps to to turn 90 degrees
+
+// The timer states.
+enum blindfold_states {
+  B_OPEN,
+  B_OPENING,
+  B_CLOSED,
+  B_CLOSING,
+  B_CALIBRATING,
+  B_TRIM_CW,
+  B_TRIM_CCW
+};
+blindfold_states blindfold_state = B_CALIBRATING;
 
 void setup() 
 { 
   DDRC = 0xFF; // set all to output
   
-  pinMode(PIN_DIRECTION, INPUT_PULLUP);
+  pinMode(PIN_TOGGLE, INPUT_PULLUP);
   
   //Serial.begin(9600);
 } 
 
+void stateRun() 
+{
+  static int step_count = 0;
+  
+  switch (blindfold_state) {
+    case B_OPEN: 
+    
+    //Serial.print("steps:" ); Serial.println(step_count);
+    
+      // Monitor the toggle switch.
+      if (toggled()) {
+        step_count = 0;
+        blindfold_state = B_CLOSING;
+      }
+      break;
+      
+    case B_OPENING:
+      // Move to the open position
+      
+      
+      if (step_count >= QUARTER_TURN) {
+        // stop
+        PORTC = 0;
+        blindfold_state = B_OPEN;
+      }
+      else {
+        step(1);
+        step_count++;
+      }
+      delay(1); // Must give time for the motor to move
+      break;
+    
+    case B_CLOSED:
+      // Monitor the toggle switch.
+      if (toggled()) {
+        step_count = 0;
+        blindfold_state = B_OPENING;
+      }
+      break;
+    
+    case B_CLOSING:
+      // Move to the closed position
+      
+      
+      if (step_count >= QUARTER_TURN) {
+        // stop
+        PORTC = 0;
+        blindfold_state = B_CLOSED;
+      } else {
+        step(0);
+        step_count++; 
+      }
+      delay(1); // Must give time for the motor to move
+      break;
+    
+    case B_CALIBRATING:
+      // for now pretend calibrated and set to closed
+      blindfold_state = B_CLOSED;
+      break;
+    
+    case  B_TRIM_CW:
+    
+      break;
+    
+    case B_TRIM_CCW:
+      
+      break;
+    
+    default:
+    
+      break;
+  }
+}
+
+/**
+ * Whether the toggle switch is pushed
+ */
+byte toggled() 
+{
+  // Switch is pulled high so active low.
+  return !digitalRead(PIN_TOGGLE);
+}
+
 void loop()
 {
-  static byte active = 1;
-  static int count = 0;
-  static byte current_dir = 0;
-  
-  byte dir = digitalRead(PIN_DIRECTION);
-  
-  
-  if (current_dir != dir) {
-    current_dir = dir;
-    count = 0; 
-    active = 1;
-  }
-  
-  if (active) {
-    
-    
-    step(dir);
-    
-    //Serial.print("steps:" );
-    //Serial.println(count);
-    count++;
-    
-    if (count == QUARTER_TURN) {
-      // stop
-      PORTC = 0;
-      active = 0;
-    }
-  }
-  
-  delay(1);
+  stateRun();
 }
 
 void step(byte dir)
